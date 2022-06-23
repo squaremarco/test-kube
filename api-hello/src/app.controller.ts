37aa01f6bfc1ebf739621e8c2068c698e3ab04ea
Marco Cesi <marco.cesi@makrshark.com>
2022-06-23 11:48:16 +0200

    Add stuff

--- api-hello/src/app.controller.ts
+++ api-hello/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/nanoid')
+  getNanoid(): string {
+    return this.appService.getNanoid();
+  }
 }
--- api-hello/src/app.service.ts
+++ api-hello/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { nanoid } from 'nanoid';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return process.env.HELLO_MESSAGE;
+  }
+
+  getNanoid(): string {
+    return nanoid();
   }
 }
